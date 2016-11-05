73f74c25dcd6873877d89ff1f47e4a028d3bb0a2
Juan Diego <juandiegp17@gmail.com>
2016-11-05 12:19:20 -0400

    Some bugs where fixed on class map. If some can make a version of the game to play 1 vs 1 on the terminal it would be great, to prove class map and try every single case.

--- map/main.C
+++ map/main.C
@@ -9,6 +9,7 @@ int main()
 	map<3,3> map_;
 
 	MOVE mv;
+
 	get<0>(get<0>(mv)) = 0;
 	get<1>(get<0>(mv)) = 1;
 
@@ -60,5 +61,122 @@ int main()
 	cout << "score player 1: " << map_.get_p1_score() << "\n";
 
 
+	get<0>(get<0>(mv)) = 0;
+	get<1>(get<0>(mv)) = 1;
 
+	get<0>(get<1>(mv)) = 0;
+	get<1>(get<1>(mv)) = 2;
+
+	if(map_.play(mv,1))
+		cout << "valid move" << "\n";
+	else
+		cout << "invalid move" << "\n";
+
+
+
+	cout << "score player 1: " << map_.get_p1_score() << "\n";
+
+	get<0>(get<0>(mv)) = 0;
+	get<1>(get<0>(mv)) = 2;
+
+	get<0>(get<1>(mv)) = 1;
+	get<1>(get<1>(mv)) = 2;
+
+	if(map_.play(mv,1))
+		cout << "valid move" << "\n";
+	else
+		cout << "invalid move" << "\n";
+
+
+
+	cout << "score player 1: " << map_.get_p1_score() << "\n";
+
+	get<0>(get<0>(mv)) = 1;
+	get<1>(get<0>(mv)) = 2;
+
+	get<0>(get<1>(mv)) = 1;
+	get<1>(get<1>(mv)) = 1;
+
+	if(map_.play(mv,1))
+		cout << "valid move" << "\n";
+	else
+		cout << "invalid move" << "\n";
+
+	cout << "score player 1: " << map_.get_p1_score() << "\n";
+
+	get<0>(get<0>(mv)) = 1;
+	get<1>(get<0>(mv)) = 2;
+
+	get<0>(get<1>(mv)) = 2;
+	get<1>(get<1>(mv)) = 2;
+
+	if(map_.play(mv,1))
+		cout << "valid move" << "\n";
+	else
+		cout << "invalid move" << "\n";
+
+	cout << "score player 1: " << map_.get_p1_score() << "\n";
+
+
+	get<0>(get<0>(mv)) = 2;
+	get<1>(get<0>(mv)) = 1;
+
+	get<0>(get<1>(mv)) = 2;
+	get<1>(get<1>(mv)) = 2;
+
+	if(map_.play(mv,1))
+		cout << "valid move" << "\n";
+	else
+		cout << "invalid move" << "\n";
+
+	cout << "score player 1: " << map_.get_p1_score() << "\n";
+
+
+	get<0>(get<0>(mv)) = 1;
+	get<1>(get<0>(mv)) = 1;
+
+	get<0>(get<1>(mv)) = 2;
+	get<1>(get<1>(mv)) = 1;
+
+	if(map_.play(mv,1))
+		cout << "valid move" << "\n";
+	else
+		cout << "invalid move" << "\n";
+
+	cout << "score player 1: " << map_.get_p1_score() << "\n";
+
+
+	get<0>(get<0>(mv)) = 1;
+	get<1>(get<0>(mv)) = 0;
+
+	get<0>(get<1>(mv)) = 2;
+	get<1>(get<1>(mv)) = 0;
+
+	if(map_.play(mv,1))
+		cout << "valid move" << "\n";
+	else
+		cout << "invalid move" << "\n";
+
+	cout << "score player 1: " << map_.get_p1_score() << "\n";
+
+
+	get<0>(get<0>(mv)) = 2;
+	get<1>(get<0>(mv)) = 0;
+
+	get<0>(get<1>(mv)) = 2;
+	get<1>(get<1>(mv)) = 1;
+
+	if(map_.play(mv,1))
+		cout << "valid move" << "\n";
+	else
+		cout << "invalid move" << "\n";
+
+	cout << "score player 1: " << map_.get_p1_score() << "\n";
+
+
+	cout << "map_full : " << map_.is_full() << "\n";
+
+	map_.reset();
+
+	cout << "after reset map_full : " << map_.is_full() << "\n";
 }
