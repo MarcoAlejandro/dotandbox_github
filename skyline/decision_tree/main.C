391c74b648090aeb4f63cf0ceeef95121eb50d88
Juan Diego <juandiegp17@gmail.com>
2016-11-10 17:03:18 -0400

    g

--- skyline/decision_tree/main.C
+++ skyline/decision_tree/main.C
@@ -2,9 +2,27 @@
 
 int main()
 {
-	map<3,3> map_;
+	map<2,2> map_;
 
 	MOVE mv;
+
+	get<0>(get<0>(mv)) = 0;
+	get<1>(get<0>(mv)) = 0;
+
+	get<0>(get<1>(mv)) = 0;
+	get<1>(get<1>(mv)) = 1;
+
+	map_.play(mv,1);
+
+
+	//map_.print_map_terminal();
+
+	
+	dotandbox_tree<2,2,3>  d_t(map_,mv);
+
+	d_t.make_decision_tree(map_,mv);
+
+	d_t.reset_tree();
 
 	get<0>(get<0>(mv)) = 0;
 	get<1>(get<0>(mv)) = 1;
@@ -14,12 +32,9 @@ int main()
 
 	map_.play(mv,1);
 
-	//map_.print_map_terminal();
+cout << "\nAFTER RESET\n";
 
-	
-	dotandbox_tree<3,3,11>  d_t(map_,mv);
-
-	d_t.make_decision_tree(map_,mv);
+	d_t.make_decision_tree(map_,mv);	
 
 	/*cout << "\n";
 	d_t.get_root()->get_map().print_map_terminal();
