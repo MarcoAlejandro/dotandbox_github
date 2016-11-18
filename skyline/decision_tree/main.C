3dabfec2177275c5f26e9f970a905e73097d2ebf
Juan Diego <juandiegp17@gmail.com>
2016-11-17 20:38:24 -0400

    some bugs were fixed.

--- skyline/decision_tree/main.C
+++ skyline/decision_tree/main.C
@@ -2,9 +2,11 @@
 
 int main()
 {
-	map<3,3> map_;
-
+	
+	map<2,2> map_;
 	MOVE mv;
+
+	dotandbox_tree<2,2,3> * d_t = new dotandbox_tree<2,2,3>(map_,mv);
 
 	get<0>(get<0>(mv)) = 0;
 	get<1>(get<0>(mv)) = 0;
@@ -12,21 +14,49 @@ int main()
 	get<0>(get<1>(mv)) = 0;
 	get<1>(get<1>(mv)) = 1;
 
+	map_.play(mv,2);
+
+
+	d_t->make_decision_tree(map_,mv,2);
+
+	for (size_t i = 0; i < 3 ; ++i)
+	{
+		cout << "---utility: " <<d_t->get_root()->get_children()[i].get_utility() << endl;
+	}
+
+	d_t->reset_tree();
+
+	get<0>(get<0>(mv)) = 0;
+	get<1>(get<0>(mv)) = 1;
+
+	get<0>(get<1>(mv)) = 1;
+	get<1>(get<1>(mv)) = 1;
+
 	map_.play(mv,1);
 
 
-	//map_.print_map_terminal();
-
+	d_t->make_decision_tree(map_,mv,1);
 	
-	dotandbox_tree<3,3,5> * d_t = new dotandbox_tree<3,3,5>(map_,mv);
-
-	d_t->make_decision_tree(map_,mv,2);
-	
-	for (size_t i = 0; i < 11 ; ++i)
+	for (size_t i = 0; i < 2 ; ++i)
 	{
 		cout << "utility: " <<d_t->get_root()->get_children()[i].get_utility() << endl;
 	}
 
+	d_t->reset_tree();
+
+
+	get<0>(get<0>(mv)) = 1;
+	get<1>(get<0>(mv)) = 0;
+
+	get<0>(get<1>(mv)) = 1;
+	get<1>(get<1>(mv)) = 1;
+
+	map_.play(mv,2);
+
+	d_t->make_decision_tree(map_,mv,2);
+
+	mv = d_t->get_next_move();
+	cout << "(" << get<0>(get<0>(mv)) << "," << get<1>(get<0>(mv)) << ") - (" << get<0>(get<1>(mv)) << "," << get<1>(get<1>(mv)) << ")" << endl;
 	/*cout << "\n";
 	d_t.get_root()->get_map().print_map_terminal();
 	cout << "\n\n";
